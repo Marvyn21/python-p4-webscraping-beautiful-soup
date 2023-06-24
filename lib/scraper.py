8efb7d992de72f1c71a578f547d9a7f556c455cd
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-06-24 09:40:39 +0300

    Completed Assignment

--- lib/scraper.py
+++ lib/scraper.py
@@ -3,4 +3,4 @@ from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
-html = requests.get("https://flatironschool.com/", headers=headers)
+html = requests.get("https://moringaschool.com/", headers=headers)
